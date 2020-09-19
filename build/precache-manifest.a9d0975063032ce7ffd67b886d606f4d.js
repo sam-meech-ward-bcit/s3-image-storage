d21b47b5679a986a8bb48ccb78741ef036398b31
Sam Meech-Ward <sam@meech-ward.com>
2020-09-19 14:08:04 -0700

    💩

---
+++ build/precache-manifest.a9d0975063032ce7ffd67b886d606f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651a699e33575e08ac669a77fdfa1089",
+    "url": "/index.html"
+  },
+  {
+    "revision": "714fb51899f6e2bd4095",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b315c84f6235def41d1",
+    "url": "/static/js/2.f4d2017b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f4d2017b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714fb51899f6e2bd4095",
+    "url": "/static/js/main.61688473.chunk.js"
+  },
+  {
+    "revision": "323797dfa579f27875ae",
+    "url": "/static/js/runtime-main.ff109875.js"
+  }
+]);
